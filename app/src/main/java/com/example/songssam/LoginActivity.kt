eb26273e226939a9bbc92033d83c7e6b72332436
Joontae <chlwnsxo0@gmail.com>
2023-07-03 17:27:22 +0900

    temp

--- app/src/main/java/com/example/songssam/LoginActivity.kt
+++ app/src/main/java/com/example/songssam/LoginActivity.kt
@@ -2,10 +2,14 @@ package com.example.songssam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.common.util.Utility
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var keyHash = Utility.getKeyHash(this)
+        Log.d("keyhash",keyHash)
     }
 }
